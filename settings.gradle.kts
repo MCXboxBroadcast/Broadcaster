8e5a54a82125ea51e30e18bd577b95d92791b9a0
rtm516 <ryantmilner@hotmail.co.uk>
2024-06-17 18:45:03 +0100

    Remove bootstrap-parent as defined project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,7 @@
-include(":bootstrap-standalone")
-include(":bootstrap-parent")
 include(":core")
+include(":bootstrap-standalone")
 include(":bootstrap-geyser")
 project(":bootstrap-standalone").projectDir = file("bootstrap/standalone")
-project(":bootstrap-parent").projectDir = file("bootstrap")
 project(":bootstrap-geyser").projectDir = file("bootstrap/geyser")
 
 pluginManagement {
