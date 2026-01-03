504e60c192fa06a70690cd271a74d59f39e1ebd0
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2026-01-02 19:09:47 -0800

    Update to Gradle 9.2.1
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,13 @@
 include(":core")
 include(":bootstrap-standalone")
 include(":bootstrap-geyser")
-include(":bootstrap-manager")
 project(":bootstrap-standalone").projectDir = file("bootstrap/standalone")
 project(":bootstrap-geyser").projectDir = file("bootstrap/geyser")
-project(":bootstrap-manager").projectDir = file("bootstrap/manager")
+
+if (file("bootstrap/manager").exists()) {
+    include(":bootstrap-manager")
+    project(":bootstrap-manager").projectDir = file("bootstrap/manager")
+}
 
 pluginManagement {
     repositories {
