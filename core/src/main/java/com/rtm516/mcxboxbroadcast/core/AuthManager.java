9695237d1115efe19c02c9fb3f101a2dd11130fa
rtm516 <ryantmilner@hotmail.co.uk>
2026-09-16 23:45:16 +0100

    Update to MinecraftAuth 5.0.2

--- core/src/main/java/com/rtm516/mcxboxbroadcast/core/AuthManager.java
+++ core/src/main/java/com/rtm516/mcxboxbroadcast/core/AuthManager.java
@@ -3,8 +3,6 @@ package com.rtm516.mcxboxbroadcast.core;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.rtm516.mcxboxbroadcast.core.exceptions.AgeVerificationException;
-import com.rtm516.mcxboxbroadcast.core.models.auth.CachedProfileInfo;
-import com.rtm516.mcxboxbroadcast.core.models.auth.XblUsersMeProfileRequest;
 import com.rtm516.mcxboxbroadcast.core.notifications.NotificationManager;
 import com.rtm516.mcxboxbroadcast.core.storage.StorageManager;
 import net.lenni0451.commons.httpclient.HttpClient;
@@ -13,8 +11,6 @@ import net.raphimc.minecraftauth.bedrock.BedrockAuthManager;
 import net.raphimc.minecraftauth.msa.model.MsaDeviceCode;
 import net.raphimc.minecraftauth.msa.service.impl.DeviceCodeMsaAuthService;
 import net.raphimc.minecraftauth.util.MinecraftAuth4To5Migrator;
-import net.raphimc.minecraftauth.util.holder.Holder;
-import net.raphimc.minecraftauth.util.holder.listener.BasicChangeListener;
 import net.raphimc.minecraftauth.util.http.exception.InformativeHttpRequestException;
 
 import java.io.IOException;
@@ -27,8 +23,6 @@ public class AuthManager {
 
     private BedrockAuthManager authManager;
     private Runnable onDeviceTokenRefreshCallback;
-
-    private final Holder<CachedProfileInfo> profileInfo;
 
     /**
      * Create an instance of AuthManager
@@ -43,12 +37,6 @@ public class AuthManager {
         this.logger = logger.prefixed("Auth");
 
         this.authManager = null;
-        this.profileInfo = new Holder<>(() -> {
-            HttpClient httpClient = MinecraftAuth.createHttpClient();
-            XblUsersMeProfileRequest.Response response = httpClient.executeAndHandle(new XblUsersMeProfileRequest(authManager.getXboxLiveXstsToken().getUpToDate()));
-            XblUsersMeProfileRequest.Response.ProfileUser profileUser = response.profileUsers().get(0);
-            return new CachedProfileInfo(profileUser.settings().get("Gamertag"), profileUser.id());
-        });
     }
 
     /**
@@ -143,7 +131,7 @@ public class AuthManager {
             // Requesting up-to-date tokens will automatically refresh them if expired
             authManager.getXboxLiveXstsToken().getUpToDate();
             authManager.getPlayFabToken().getUpToDate();
-            profileInfo.getUpToDate();
+            authManager.getXboxUserProfile().getUpToDate();
         } catch (InformativeHttpRequestException e) {
             if (e.getMessage().contains("agecheck")) {
                 throw new AgeVerificationException("Authentication failed due to age verification requirement", e);
@@ -213,7 +201,7 @@ public class AuthManager {
      * @return The Gamertag of the current user
      */
     public String getGamertag() {
-        return profileInfo.getCached().gamertag();
+        return authManager.getXboxUserProfile().getCached().getSettings().get("Gamertag");
     }
 
     /**
@@ -222,6 +210,6 @@ public class AuthManager {
      * @return The XUID of the current user
      */
     public String getXuid() {
-        return profileInfo.getCached().xuid();
+        return authManager.getXboxUserProfile().getCached().getId();
     }
 }
